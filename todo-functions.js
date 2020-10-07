230d531005ce5d12b9f6e961f1a339a0d9bae9de
Baz07 <67526825+Baz07@users.noreply.github.com>
2020-10-07 16:43:54 -0400

    Function to fetch todos from local storage!

--- todo-functions.js
+++ todo-functions.js
@@ -1,1 +1,10 @@
-
+// Function 1: Get Notes from local Storage (If none is present, create a new array)
+const getSavedNotes = function () {
+    const todosData = localStorage.getItem('todos')
+    // console.log(`Initially: ${todosData}`)
+    if (todosData != null) {
+        return JSON.parse(todosData)
+    } else {
+        return []
+    }
+}
