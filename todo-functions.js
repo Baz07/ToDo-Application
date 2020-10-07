947ab875a81825ed5b1c9ac7630bd2a521c30efb
Baz07 <67526825+Baz07@users.noreply.github.com>
2020-10-07 16:57:48 -0400

    Render Todos Sub Functions & remove todo function.

--- todo-functions.js
+++ todo-functions.js
@@ -36,3 +36,70 @@ const renderTodos = function (todos, filters) {
     })
 
 }
+
+//Function 4: Generate structure of TODO DOM
+const generateTodoDOM = function (todo) {
+    
+    //Create Container Space
+    const todoSpace = document.createElement('div')
+
+    // Create Remove Button
+    const button = document.createElement('button')
+    button.setAttribute('class', "btn btn-outline-danger")
+    button.style.paddingTop = '5px'
+    button.style.paddingBottom = '5px'
+    button.style.margin = '5px'
+    button.style.fontSize = 'small'
+
+    // Create Space for Todo Text
+    const todoText = document.createElement('span')
+
+    const breaker = document.createElement('br')
+
+    // Place a Remove Button
+    button.textContent = "\nKill Task"
+    todoSpace.appendChild(button)
+    button.addEventListener('click', function () {
+        let todos = removeTodo(todo.todo_id)
+        // console.log(todos)
+        saveTodo(todos)
+        renderTodos(todos, filters)
+    })
+
+    // Set Text Content as Todo Text
+    todoText.textContent = `P${todo.todo_priority}: ${todo.todo_name}`
+    todoSpace.appendChild(todoText)
+
+    //Setup a Breaker
+    todoSpace.appendChild(breaker)
+
+    return todoSpace
+}
+
+//Function 5: Summary of TODO DOM
+const generateSummaryDOM = function (incompletedTodos) {
+    const summarySpace = document.createElement('div')
+    const notification = document.createElement('h3')
+    notification.textContent = `Tasks left: ${incompletedTodos.length}`
+    return notification
+}
+
+//Function 6: Remove Todo
+const removeTodo = function (id) {
+    
+    //Need to find Todo Index
+    const todoIndex = todos.findIndex(function(todo){
+        return todo.todo_id === id
+    })
+    
+    // Remove Index if >= 0 
+    if (todoIndex > -1){
+        // console.log(`Before emvoing ${todos}`)
+        // console.log("Remove Start")
+        todos.splice(todoIndex, 1)
+        // console.log("Removed Success")
+        // console.log(`After remvoing ${todos}`)
+     }
+     
+     return todos
+}
