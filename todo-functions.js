c5d8b848e23132d46fb85be52c9a2d3d5f7318a0
Baz07 <67526825+Baz07@users.noreply.github.com>
2020-10-07 16:51:10 -0400

    Render Todos Function!

--- todo-functions.js
+++ todo-functions.js
@@ -13,3 +13,26 @@ const getSavedNotes = function () {
 const saveTodo = function (todos) {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
+
+// Function 3: Render Todos
+const renderTodos = function (todos, filters) {
+    console.log(`Render Todos: ${todos}`)
+    const filteredTodos = todos.filter(function (todo) {
+        const matchTodos = todo.todo_name.toLowerCase().includes(filters.searchText.toLowerCase())
+        return matchTodos
+    })
+    
+    const incompletedTodos = filteredTodos.filter(function (todo) {
+        return !todo.todo_complete_status
+    })
+
+    // Fetch the HTML Element and set its value to an empty String
+    document.querySelector('#show-todo').innerHTML = ''
+
+    //Notificaton of Incompleted Todos
+    document.querySelector('#show-todo').appendChild(generateSummaryDOM(incompletedTodos))
+    filteredTodos.forEach(function (todo) {
+        document.querySelector('#show-todo').appendChild(generateTodoDOM(todo))
+    })
+
+}
